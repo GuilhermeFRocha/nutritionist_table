bffcc6f39fea9f9c6da61a6996bb42ebb2a91ca8
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2021-05-20 18:47:55 -0300

    Adicionando validaçao de formulario e filtraçao de tabela

--- js/form.js
+++ js/form.js
@@ -1,26 +1,36 @@
-let botaoAdicionar = document.querySelector("#adicionar-paciente")
-botaoAdicionar.addEventListener("click", function (event) {
+var botaoAdicionar = document.querySelector("#adicionar-paciente")
+    botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault()
-    let form = document.querySelector("#form-adiciona")
 
-   let paciente =  obtemPacienteDoFormulario(form)
+    var form = document.querySelector("#form-adiciona")
+    var paciente =  obtemPacienteDoFormulario(form)
+    var pacienteTr = montaTr(paciente)
+    var erros = validaPaciente(paciente)
 
-
-    let pacienteTr = montaTr(paciente)
-
-    if (!validaPaciente(paciente)){
-        
+    if (erros.length > 0 ){
+        exibeMensagensDeErro(erros)
+        return
     }
 
-    let tabela = document.querySelector("#tabela-pacientes")
+    var tabela = document.querySelector("#tabela-pacientes")
     tabela.appendChild(pacienteTr)
 
     form.reset()
-
+    var mensagensErro = document.querySelector("#mensagens-erro")
+    mensagensErro.innerHTML = ""
 })
-    function obtemPacienteDoFormulario(form) {
+    function exibeMensagensDeErro(erros){
+        var ul = document.querySelector("#mensagens-erro") 
+        ul.innerHTML = ""
+        erros.forEach(function(erro){
+            var li = document.createElement("li")
+            li.textContent = erro
+            ul.appendChild(li)
+        })
+    }
 
-        let paciente = {
+    function obtemPacienteDoFormulario(form) {
+        var paciente = {
             nome: form.nome.value,
             peso: form.peso.value,
             altura: form.altura.value,
@@ -32,46 +42,56 @@ botaoAdicionar.addEventListener("click", function (event) {
 
     function montaTr(paciente){
 
-    let pacienteTr = document.createElement("tr")
+    var pacienteTr = document.createElement("tr")
     pacienteTr.classList.add("paciente")
 
-    
-
-    let nomeTd = montaTd(paciente.nome,"nome-peso")
-    let pesoTd = montaTd(paciente.peso,"info-peso")
-    let alturaTd = montaTd(paciente.altura,"info-altura")
-    let gorduraTd = montaTd(paciente.gordura,"info-gorduraTd")
-    let imcTd = montaTd(paciente.imc,"info-imc")
+    var nomeTd = montaTd(paciente.nome,"nome-peso")
+    var pesoTd = montaTd(paciente.peso,"info-peso")
+    var alturaTd = montaTd(paciente.altura,"info-altura")
+    var gorduraTd = montaTd(paciente.gordura,"info-gorduraTd")
+    var imcTd = montaTd(paciente.imc,"info-imc")
 
     pacienteTr.appendChild(nomeTd)
     pacienteTr.appendChild(pesoTd)
     pacienteTr.appendChild(alturaTd)
     pacienteTr.appendChild(gorduraTd)
     pacienteTr.appendChild(imcTd)
-
     return pacienteTr
-
     }
 
 function montaTd(dado,classe) {
-    let td = document.createElement("td")
-    td.textContent =dado
+    var td = document.createElement("td")
+    td.textContent = dado
     td.classList.add(classe)
-
     return td
 }
 
-    let pesoTd = document.createElement("td")
-    let alturaTd = document.createElement("td")
-    let gorduraTd = document.createElement("td")
-    let imcTd = document.createElement("td")
-
-
+    var pesoTd = document.createElement("td")
+    var alturaTd = document.createElement("td")
+    var gorduraTd = document.createElement("td")
+    var imcTd = document.createElement("td")
 
     function validaPaciente (paciente){
-        if (validaPeso(paciente.peso)){
-            return true
-        }else {
-            return false
+   
+        var erros = []
+
+        if(paciente.nome.length == 0){
+            erros.push("O nome não pode ser em branco")
         }
+        if (!validaPeso(paciente.peso)){
+            erros.push("Peso Invalido")
+        }
+        if (!validaAltura(paciente.altura)){
+            erros.push("Altura Invalida")
+        }
+        if(paciente.gordura.length == 0) {
+            erros.push("A gordura não pode ser em branco")
+        }
+        if(paciente.altura.length == 0) {
+            erros.push("A altura não pode ser em branco")
+        }
+        if(paciente.peso.length == 0) {
+            erros.push("O peso não pode ser em branco")
+        }
+        return erros
     }
